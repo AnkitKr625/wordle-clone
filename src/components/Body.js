8fd90e56d27d29fb5c1fcdc29f10259f095b9350
Ankit Kumar <ankitkr1009@gmail.com>
2022-09-27 10:36:53 +0530

    word-input

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,36 @@
-import {useState, useRef, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import WordInput from './WordInput';
 
 function Body() {
+  const wordOftheDay = 'APPLE';
   const[wordArray, setWordArray] = useState([]);
   const[word, setWord] = useState('');
-  const inputRef = useRef();
+  const currWord = useRef('');
+  const currWordArr = useRef('');
+  const [foundWordArr, setFoundWordArr] = useState([]); 
   
   function handleInput(word) {
     const newWord = word.trim().substring(0,5).toUpperCase();
     setWord(newWord);
   }
 
-  function handleEnterKey(e) {
-    if(!e || e.charCode === 13) {
-      e?.preventDefault();
-      if(word.length === 5) {
-        let newWordArr = [...wordArray, word];
-        setWordArray(newWordArr);
+  function keyboardInput(e) {
+    let c = e.charCode;
+    if(c === 13) {
+      e.preventDefault();
+      handleEnterKey();
+    } else if((c > 64 && c < 91) || (c > 96 && c < 123)) {
+      handleKeyPress(e.key.toUpperCase());
+    }
+  }
+
+  function handleEnterKey() {
+    let cw = currWord.current;
+    let cwa = currWordArr.current;
+    if(cw.length === 5) {
+      if(cwa.length < 6) {
+        setWordArray([...cwa, cw]);
         setWord('');
-        inputRef.current.focus();
       }
     }
   }
@@ -39,22 +51,49 @@ function Body() {
     })
   }
 
+  function handleKeyDown(e) {
+    let keyCode = e.keyCode;
+    if(keyCode === 8) {
+      setWord(prevWord => prevWord.substring(0, prevWord.length -1));
+    } else if (keyCode > 64 && keyCode < 91) {
+      handleKeyPress(e.key.toUpperCase());
+    } else if(keyCode === 13) {
+      handleEnterKey();
+    }
+  }
+
   useEffect(() => {
-    inputRef.current.focus();
-  })
+    currWord.current = word;
+  },[word]);
+
+  useEffect(() => {
+    currWordArr.current = [...wordArray];
+    let newWord = wordArray[wordArray.length - 1];
+    let fndArr = [false, false, false, false, false];
+    for(let i=0;i<5;++i){
+      if(wordOftheDay.charAt(i) === newWord?.charAt(i)) {
+        fndArr[i] = true;
+      }
+    }
+  },[wordArray])
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  },[]);
 
   return (
     <>
     <div className='flex-column-center'>
       { [1,2,3,4,5,6].map((e,i) => {
         return (
-          <>
-            <WordInput word={wordArray[i]} key={e}/>
-          </>
+          <WordInput word={wordArray.length + 1 >= e ? wordArray[i] || word : ''} key={e}/>
         )
       })}
     </div>
-    <input ref={inputRef} type="text" value={word} onChange={(e) => handleInput(e.target.value)} onKeyPress={(e) => handleEnterKey(e)}/>
     <div className='flex-center'>
       {['q','w','e','r','t','y','u','i','o','p'].map((l) => {
         return (
