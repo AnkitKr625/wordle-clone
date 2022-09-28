72566b1fab33d3da9e15f4ec25da3add00c6c097
Ankit Kumar <ankitkr1009@gmail.com>
2022-09-29 00:04:06 +0530

    transition effect added

--- src/components/Body.js
+++ src/components/Body.js
@@ -209,14 +209,14 @@ function Body() {
       <div className='flex-center'>
         {keys.slice(0,10).map(({key, bgColor}) => {
           return (
-            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
+            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor, transition: '3s'}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
           )
         })}
       </div>
       <div className='flex-center'>
         {keys.slice(10,19).map(({key, bgColor}) => {
           return (
-            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
+            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor, transition: '3s'}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
           )
         })}
       </div>
@@ -224,11 +224,11 @@ function Body() {
         <div className="flex-center key cursor-pointer" style={{backgroundColor: '#8f9396bf'}} onClick={() => handleEnterKey()}>{'ENTER'}</div>
         {keys.slice(19).map(({key, bgColor}) => {
           return (
-            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
+            <div className='flex-center key cursor-pointer' style={{backgroundColor: bgColor, transition: '3s'}} onClick={() => handleKeyPress(key)} key={key}>{key}</div>
           )
         })}
         <div className="flex-center key cursor-pointer" style={{backgroundColor: '#8f9396bf'}} onClick={() => handleBackspaceKey()}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" data-testid="icon-backspace"><path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="48" data-testid="icon-backspace"><path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path></svg>
         </div>
       </div>
     </div>
--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -5,7 +5,7 @@ function WordInput({ word, bgColor = [] }) {
       <div className='flex-center'>
         { [1,2,3,4,5].map((e,i) => {
           return (
-            <div className='letter-box flex-center' key={word+" "+e} style={{backgroundColor: bgColor[i]}}> {word?.charAt(i) ? word.charAt(i) : ''} </div>
+            <div className='letter-box flex-center' key={word+" "+e} style={{backgroundColor: bgColor[i], transition: '3s'}}> {word?.charAt(i) ? word.charAt(i) : ''} </div>
           )
         })}
       </div> 
